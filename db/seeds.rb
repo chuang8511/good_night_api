3da4e8772359dd238789ea2184e7056339e88766
chuang <chuang@netprotections.co.jp>
2022-12-04 15:02:35 +0800

    fake data to test in local

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,5 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+ClickInRecord.create(name: "fake_user_1")
+ClickInRecord.create(name: "fake_user_2")
